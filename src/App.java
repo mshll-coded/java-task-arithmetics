82471a90d5637cbbec940325b0e2ef201d17979b
meshal <meshal7@pdx.edu>
2024-11-03 10:56:44 +0300

    complete task

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        double amountKWD = 86;
+        System.out.println(amountKWD + " KWD = " + (amountKWD * 3.25) + " USD");
+
+        double amountUSD = 279.5;
+        System.out.println(amountUSD + " USD = " + (amountUSD / 3.25) + " KWD");
+
+    }
+}
